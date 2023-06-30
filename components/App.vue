a88614259b46230d51d95d9d849b4fe069625ea1
Darlan Alves <me@darlanalv.es>
2023-06-30 13:52:24 +0200

    chore: add header input

--- components/App.vue
+++ components/App.vue
@@ -1,72 +1,88 @@
 <template>
-  <div class="container mx-auto px-4 relative">
-    <div v-if="loading" class="mx-auto p-2">
-      <span class="material-icons text-sm text-green-500 animate-spin"
-        >sync</span
-      >
+  <div class="py-4">
+    <div v-if="loading" class="absolute top-0 right-0 m-2">
+      <span class="material-icons text-green-500 animate-spin">sync</span>
     </div>
-    <div class="flex mb-4">
-      <input
-        v-model="search"
-        type="text"
-        placeholder="Filter..."
-        class="px-3 py-2 border rounded w-full flex-grow"
-      />
-      <button class="p-2 bg-gray-300 rounded-sm leading-4" @click="fetchAll()">
-        <span class="material-icons">refresh</span>
-      </button>
-    </div>
-    <div
-      v-if="!loading"
-      class="grid justify-items-stretch gap-4 grid-cols-2 md:grid-cols-3 mb-4"
-    >
-      <div
-        class="relative border border-gray-300 round bg-white shadow p-3 rounded-md overflow-y-auto h-30"
-        v-for="bot in list"
-        :key="bot.name"
-        @click="editBot(bot)"
-      >
-        <button
-          class="absolute top-0 right-0 m-2 text-red-500"
-          @click="removeBot(bot.name)"
-        >
-          <span class="material-icons">delete</span>
-        </button>
-        <h2 class="text-md font-bold flex-grow">{{ bot.name }}</h2>
-        <p>{{ bot.header }}</p>
-      </div>
-    </div>
-
-    <form
-      class="flex flex-col border border-gray-300 round bg-white shadow p-3 rounded-md"
-      @submit.prevent="addBot()"
-    >
-      <label class="block mb-4">
-        <span class="block uppercase text-xs font-medium text-gray-700">
-          Name:
-        </span>
+    <div class="container mx-auto px-4">
+      <div class="flex mb-4 rounded shadow">
         <input
+          v-model="search"
           type="text"
-          v-model="newBot"
-          class="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm"
+          placeholder="Filter..."
+          class="px-3 py-2 border rounded w-full flex-grow"
         />
-      </label>
-      <label class="block mb-4">
-        <span class="block uppercase text-xs font-medium text-gray-700">
-          System instruction:
-        </span>
-        <textarea
-          type="text"
-          v-model="newBotHeader"
-          class="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm h-40"
-        ></textarea>
-      </label>
-      <div class="text-right">
-        <button class="py-2 px-4 bg-blue-500 text-white rounded" type="submit">
+        <button
+          class="p-2 bg-gray-300 rounded-sm leading-4"
+          @click="fetchAll()"
+        >
+          <span class="material-icons">refresh</span>
+        </button>
+      </div>
+      <div
+        v-if="!loading"
+        class="grid justify-items-stretch gap-4 grid-cols-1 md:grid-cols-2 mb-4"
+      >
+        <div
+          class="relative border border-gray-300 round bg-white shadow p-3 rounded-md overflow-y-auto h-30"
+          v-for="bot in list"
+          :key="bot.name"
+        >
+          <div class="absolute top-0 right-0 m-2">
+            <button class="text-red-500" @click="removeBot(bot.name)">
+              <span class="material-icons">delete</span>
+            </button>
+            <button class="text-blue-500" @click="editBot(bot)">
+              <span class="material-icons">edit</span>
+            </button>
+          </div>
+          <h2 class="text-md font-bold flex-grow">{{ bot.name }}</h2>
+          <p>{{ bot.header }}</p>
+        </div>
+      </div>
+
+      <div class="text-center" v-if="!adding">
+        <button
+          class="py-2 px-4 bg-blue-500 text-white rounded"
+          @click="showForm()"
+        >
           Add
         </button>
       </div>
-    </form>
+      <form
+        v-if="!loading && adding"
+        class="flex flex-col border border-gray-300 round bg-white shadow p-3 rounded-md"
+        @submit.prevent="saveBot()"
+      >
+        <label class="block mb-4">
+          <span class="block uppercase text-xs font-medium text-gray-700">
+            Name:
+          </span>
+          <input
+            type="text"
+            v-model="newBot"
+            class="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm"
+          />
+        </label>
+        <label class="block mb-4">
+          <span class="block uppercase text-xs font-medium text-gray-700">
+            System instruction:
+          </span>
+          <textarea
+            type="text"
+            v-model="newBotHeader"
+            class="mt-1 p-2 block w-full rounded-md border border-gray-300 shadow-sm h-40"
+          ></textarea>
+        </label>
+        <div class="text-right">
+          <button
+            class="py-2 px-4 bg-blue-500 text-white rounded"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -82,6 +98,7 @@ const search = ref("");
 const newBot = ref("");
 const newBotHeader = ref("");
 const loading = ref(true);
+const adding = ref(false);
 
 ready.then(async () => {
   await fetchAll();
@@ -100,7 +117,11 @@ const list = computed(() => {
   );
 });
 
-async function addBot() {
+function showForm() {
+  adding.value = true;
+}
+
+async function saveBot() {
   const name = newBot.value;
   const header = newBotHeader.value;
 
